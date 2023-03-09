484cb28e2f859ea68366a0e4ccaabfe2ff22811c
Sax59 <waquetv@gmail.com>
2023-03-09 11:15:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import "./App.css";
+import Wilder from "./components/Wilder";
+
+const wildersData = [
+  {
+    name: "Ringo",
+    city: "London",
+    skills: [
+      { title: "JS", votes: 10 },
+      { title: "React", votes: 8 },
+    ],
+  },
+  {
+    name: "John",
+    city: "Paris",
+    skills: [
+      { title: "PHP", votes: 9 },
+      { title: "Symfony", votes: 9 },
+    ],
+  },
+  {
+    name: "George",
+    city: "Berlin",
+    skills: [
+      { title: "Ruby", votes: 10 },
+      { title: "JS", votes: 8 },
+    ],
+  },
+  {
+    name: "Paul",
+    city: "Reims",
+    skills: [
+      { title: "C++", votes: 10 },
+      { title: "Rust", votes: 8 },
+    ],
+  },
+];
+
+const App = () => {
+  console.log("hello");
+  return (
+    <div>
+      <header>
+        <div className="container">
+          <h1>Wilders Book</h1>
+        </div>
+      </header>
+      <main className="container">
+        <h2>Wilders</h2>
+        <section className="card-row">
+          {wildersData.map((wilder) => {
+            return <Wilder name={wilder.name} skills={wilder.skills} />;
+          })}
+        </section>
+      </main>
+      <footer>
+        <div className="container">
+          <p>&copy; 2022 Wild Code School</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default App;

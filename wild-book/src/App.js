2b90f95a314da1f28ca7a8af0129a869befd1544
Sax59 <waquetv@gmail.com>
2023-03-09 12:20:35 +0100

    connect backend with frontend

--- wild-book/src/App.js
+++ wild-book/src/App.js
@@ -1,42 +1,15 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import Wilder from "./components/wilder";
-
-const wildersData = [
-  {
-    name: "Ringo",
-    city: "London",
-    skills: [
-      { title: "JS", votes: 10 },
-      { title: "React", votes: 8 },
-    ],
-  },
-  {
-    name: "John",
-    city: "Paris",
-    skills: [
-      { title: "PHP", votes: 9 },
-      { title: "Symfony", votes: 9 },
-    ],
-  },
-  {
-    name: "George",
-    city: "Berlin",
-    skills: [
-      { title: "Ruby", votes: 10 },
-      { title: "JS", votes: 8 },
-    ],
-  },
-  {
-    name: "Paul",
-    city: "Reims",
-    skills: [
-      { title: "C++", votes: 10 },
-      { title: "Rust", votes: 8 },
-    ],
-  },
-];
+import axios from "axios";
 
 function App() {
+  const [wilder, setWilder] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/wilder").then((data) => {
+      setWilder(data.data);
+    });
+  }, []);
   return (
     <div className="App">
       <header>
@@ -47,7 +20,8 @@ function App() {
       <main className="container">
         <h2>Wilders</h2>
         <section className="card-row">
-          {wildersData.map((wilder) => {
+          {wilder.map((wilder) => {
+            console.log(wilder);
             return <Wilder name={wilder.name} skills={wilder.skills} />;
           })}
         </section>
